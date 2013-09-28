1fcb90be80bfd9f4166b27fdcd174bfd8d403db0
Virginia <v.grande.castro@gmail.com>
2013-09-29 00:10:03 +0200

    Added Create Discussion Window, no logic behind yet

---
+++ Localization/Localization.enUS.lua
@@ -0,0 +1,15 @@
+------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------
+---------------------------------- ColLab for Wow ----------------------------------------
+------------------------------ Uppsala University 2013 -----------------------------------
+------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------
+
+ColLabWoWLocalization = { }
+ColLabWoWLocalization["SELECT_GROUP"] = "Select group"
+ColLabWoWLocalization["SCOPE"] = "Which group will be participating in the discussion?"
+ColLabWoWLocalization["SELECT_KIND"] = "Select kind"
+ColLabWoWLocalization["KIND"] = "What kind of discussion will it be?"
+ColLabWoWLocalization["TEMPLATE"] = "Would you like to use a template?"
+ColLabWoWLocalization["YES"] = "Yes"
+ColLabWoWLocalization["NO"] = "No"
