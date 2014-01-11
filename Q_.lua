f643969c6c28dac9954e2b9680b9d8a6cff38a7f
Jason Marmon <jtmarmon@gmail.com>
2014-01-11 10:07:19 -0500

    Fixed Q__.lua

--- Q_.lua
+++ Q_.lua
@@ -1,38 +1,62 @@
 local addonName, addon = ...
- 
+ local TEXT  = true
+ local BUTTON = false
 function addon:getQTable(Qtitle)
- 
-    if(Qtitle == "Default") then
+ ------------------------------------------------------------------------------------------------------------------------
+    if(Qtitle == "Default") then -- STEP 1 - CHANGE TITLE
         local questionTable = {
-            "Question 123123",
-            "Question 2",
-            "Question 3",
-            "Question 4",
- 
+            [1] = "Question 1", -- STEP 2 - CHANGE EACH QUESTION
+            [2] =  "Question 2",
+            [3] = " Question 3",
+            [4] =  "Question 4",
+            textNums = 
+              {
+                [1] = TEXT, -- STEP 3 (Last step) - CHANGE INDIVIDUAL QUESTION TYPE (TEXT or BUTTON). ADD MORE OR TAKE AWAY SOME IF NEEDED
+                [2] = BUTTON,
+                [3] = TEXT,
+                [4] = BUTTON, 
+
+              }
             }
+         
         local returnTable = 
         {
         	title = Qtitle,
         	numQuestions = #questionTable,
         	qTable = questionTable,
         }
+        if(#returnTable.qTable.textNums ~= #returnTable.qTable) then
+          print("Warning: #returnTable.textNums ~= #returnTable.questionTable")
+        end
         return returnTable
     end
-    if(Qtitle == "Secondary") then
-           local questionTable = {
-               "Secondary 1",
-               "Secondary 2",
-               "Secondary 3",
-               "Secondary 4",
-               "Secondary 5",
- 
-               }
-          local returnTable = 
-      	 	{
-        	title = Qtitle
-        	numQuestions = #questionTable,
-        	qTable = questionTable,
-        	}
-           return returnTable
-       end
+------------------------------------------------------------------------------------------------------------------------
+    if(Qtitle == "Secondary") then -- STEP 1 - CHANGE TITLE
+        local questionTable = {
+            [1] = "Secondary Question 1", -- STEP 2 - CHANGE EACH QUESTION
+            [2] =  "Secondary Question 1",
+            [3] = "Secondary Question",
+            [4] =  "Secondary Question",
+            textNums = 
+              {
+                [1] = TEXT, -- STEP 3 (Last step) - CHANGE INDIVIDUAL QUESTION TYPE (TEXT or BUTTON).  ADD MORE OR TAKE AWAY SOME IF NEEDED
+                [2] = TEXT,
+                [3] = TEXT,
+                [4] = TEXT,
+
+              }
+            }
+         
+        local returnTable = 
+        {
+          title = Qtitle,
+          numQuestions = #questionTable,
+          qTable = questionTable,
+        }
+        if(#returnTable.qTable.textNums ~= #returnTable.qTable) then
+          print("Warning: #returnTable.textNums ~= #returnTable.questionTable")
+        end
+        return returnTable
+    end
+------------------------------------------------------------------------------------------------------------------------
 end
